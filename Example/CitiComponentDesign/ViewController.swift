df32c1a749f89f35ecf341892c9d6031f5c50fa8
David Viloria Ortega <davidviloriaortega@Davids-MacBook-Pro.local>
2022-08-03 19:26:05 -0500

    Initial commit

---
+++ Example/CitiComponentDesign/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CitiComponentDesign
+//
+//  Created by David Viloria on 08/03/2022.
+//  Copyright (c) 2022 David Viloria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
